5c2a229abaaec8e3174bcc084057c582d584f623
maria.beatriz <maria.beatriz@usto.re>
2020-04-19 13:31:01 -0300

    refatoração na lógica de tratamento da key

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ key = key.toUpperCase();
 palavra = palavra.toUpperCase();
 
 //TRATANDO O TAMANHO DA VARIAVEL KEY PARA
-   
 
-    if (palavra.length < key.length) {
-        key = key.substr(0, palavra.length)
-        console.log(key.toUpperCase());
-    };
+
+if (palavra.length < key.length) {
+    key = key.substr(0, palavra.length)
+    console.log(key.toUpperCase());
+};
 
 let newKey = [];
 var newSentence = String();
-for (i = 0; i < key.length;i++) {
+for (i = 0; i < key.length; i++) {
     let position = key.charCodeAt(i) - REFERENCE_START;
     let charCode = palavra.charCodeAt(i) + position;
-    if(charCode > 90){
+    if (charCode > 90) {
         REFERENCE_START + (charCode - REFERENCE_END);
     }
     newSentence = newSentence.concat();
@@ -28,19 +28,22 @@ for (i = 0; i < key.length;i++) {
 
 
 console.log(newSentence);
-
-function correctKeySize(key, sentence){
-    if (sentence.length > key.length) {
-        let count = 0;
-        while (sentence.length > key.length) {
-            key = key.concat(key.charAt(count));
-            count++;
-        }
-    };
+correctKeySize(key,palavra);
+function correctKeySize(key, sentence) {
+    key = key.split(" ").join();
+    let newKey = sentence.split("").reduce((accumulator, letter, index)=>{
+        index = key.length - 1 < index ? 0 : index
+        accumulator = (letter == " " ? accumulator.concat(" ") : 
+        accumulator.concat(key.charAt(index)));
+        return accumulator;
+    }, "");
+    return newKey;
 }
 
 
- 
+
+
+
 
 
 
