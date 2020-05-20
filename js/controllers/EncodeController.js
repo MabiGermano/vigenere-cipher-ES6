29d72fd052fd200e4fe03383e26d7cac6bd4d405
maria.beatriz <maria.beatriz@usto.re>
2020-05-20 20:31:35 -0300

    controller com lógica de encode

--- js/controllers/EncodeController.js
+++ js/controllers/EncodeController.js
@@ -1,29 +1,30 @@
 class EncodeController {
+    
     constructor() {
-        const REFERENCE_START = "A".charCodeAt(0) - 1;
-        const REFERENCE_END = "Z".charCodeAt(0) + 1;
+        this._REFERENCE_START = "A".charCodeAt(0) - 1;
+        this._REFERENCE_END = "Z".charCodeAt(0) + 1;
 
         let docSelect = document.querySelector.bind(document);
-        this._inputSentence = docSelect("#sentence").toUpperCase();
+        this._inputPhrase = docSelect("#phrase").toUpperCase();
         this._inputKey = docSelect("#key").toUpperCase();
     }
 
     //melhorar esse código
     encrypt() {
         event.preventDefault();
-        let encode = new Encode(this._inputKey, this._inputSentence);
+        let encode = new Encode(this._inputKey, this._inputPhrase);
         encode.correctKeySize();
 
-        var newSentence = String();
+        var newPhrase = String();
         for (i = 0; i < encode.key().length; i++) {
-            let position = encode.key().charCodeAt(i) - REFERENCE_START;
-            let charCode = encode.sentence().charCodeAt(i) + position;
+            let position = encode.key().charCodeAt(i) - this._REFERENCE_START;
+            let charCode = encode.phrase().charCodeAt(i) + position;
             if (charCode > 90) {
-                newSentence = REFERENCE_START + (charCode - REFERENCE_END);
+                newPhrase = this._REFERENCE_START + (charCode - this._REFERENCE_END);
             } else {
-                newSentence = newSentence.concat();
+                newPhrase = newPhrase.concat();
             }
         };
-        return newSentence;
+        return newPhrase;
     }
 }
