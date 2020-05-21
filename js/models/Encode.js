1aa9886b741122536a8ebf181b2b95ac68058807
maria.beatriz <maria.beatriz@usto.re>
2020-05-21 00:29:09 -0300

    corrigindo lógica de correção para tamnho da key

--- js/models/Encode.js
+++ js/models/Encode.js
@@ -1,7 +1,7 @@
 class Encode{
     constructor(key, phrase){
-        this._key = key;
-        this._phrase = phrase;
+        this._key = key.toUpperCase();
+        this._phrase = phrase.toUpperCase();
         Object.freeze();
     }
     get key(){
@@ -13,16 +13,17 @@ class Encode{
 
     correctKeySize() {
         this._key = this._key.split(" ").join("");
+
         let newKey = this._phrase.split("").reduce((accumulator, letter)=>{
-            return letter == " " ? accumulator = accumulator.concat(" ") : 
-            accumulator = accumulator.concat(this._getNextKeyChar);
-        }, new String());
-        return newKey;
+            return letter == " " ? accumulator = accumulator.concat(letter) :
+            accumulator = accumulator.concat(this._getNextKeyChar());
+        },  String());
+        this._key = newKey;
     }
 
-    _getNextKeyChar(key){
-        let firstChar = key.substr(0,1);
-        this._key = key.substr(1 - key.length).concat(firstChar); 
+    _getNextKeyChar(){
+        let firstChar = this._key.substr(0,1);
+        this._key = this._key.substr(1 - this._key.length).concat(firstChar); 
         return firstChar;
     }
 }
