f47563ddd70d5fd48d31bf951c9e42fb1600197b
maria.beatriz <maria.beatriz@usto.re>
2020-05-21 00:29:51 -0300

    melhorando código para codificar apenas letras e não caracteres especiais

--- js/controllers/EncodeController.js
+++ js/controllers/EncodeController.js
@@ -5,26 +5,53 @@ class EncodeController {
         this._REFERENCE_END = "Z".charCodeAt(0) + 1;
 
         let docSelect = document.querySelector.bind(document);
-        this._inputPhrase = docSelect("#phrase").toUpperCase();
-        this._inputKey = docSelect("#key").toUpperCase();
+        this._inputPhrase = docSelect("#phrase");
+        this._inputKey = docSelect("#key");
+        this._inputType = docSelect("#type");
+        this._message = new Message();
+        this._componentMessage = new CodedMessageView(docSelect('#resultField'));
     }
 
-    //melhorar esse código
-    encrypt() {
+    cryptography(event){
+        console.log(this);
         event.preventDefault();
-        let encode = new Encode(this._inputKey, this._inputPhrase);
-        encode.correctKeySize();
+        this._message.message = this._encrypt();
+        this._componentMessage.update(this._message);
+        this._cleanForm();
+    }
 
-        var newPhrase = String();
-        for (i = 0; i < encode.key().length; i++) {
-            let position = encode.key().charCodeAt(i) - this._REFERENCE_START;
-            let charCode = encode.phrase().charCodeAt(i) + position;
-            if (charCode > 90) {
-                newPhrase = this._REFERENCE_START + (charCode - this._REFERENCE_END);
-            } else {
-                newPhrase = newPhrase.concat();
+    //FIXME: melhorar esse código
+    _encrypt() {
+        event.preventDefault();
+        let encode = new Encode(this._inputKey.value, this._inputPhrase.value);
+        encode.correctKeySize();
+        console.log(encode);
+        let newPhrase = String();
+
+        let regexp = new RegExp(/[^a-zA-Z0-9]+/g);
+        for (let i = 0; i < encode.key.length; i++) {
+            if(regexp.test(encode.phrase.charAt(i))){
+                newPhrase = newPhrase.concat(encode.phrase.charAt(i));
+            }else{
+                let position = encode.key.charCodeAt(i) - this._REFERENCE_START;
+                let charCode = String(encode.phrase.charCodeAt(i) + position);
+                if (charCode > 90) {
+                    newPhrase = newPhrase.concat(String.fromCharCode(this._REFERENCE_START + (charCode - this._REFERENCE_END)));
+                } else {
+                    newPhrase = newPhrase.concat(String.fromCharCode(charCode));
+                }
             }
         };
         return newPhrase;
+    }
+
+    _cleanForm(){
+        this._inputPhrase.value = "";
+        this._inputKey.value = "";
+        this._inputType.value = "encode";
+
+        this._inputPhrase.focus();
+
+
     }
 }
